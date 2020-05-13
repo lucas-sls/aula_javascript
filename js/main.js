b8f173a3c7c69703ee86e00a33f10b10c9773c1d
Lucas Fonseca Muller <lucasfm2010@hotmail.com>
2020-05-12 23:00:21 -0300

    Treinando comandos básicos de Javascript!

---
+++ js/main.js
@@ -0,0 +1,72 @@
+//alert("Meu primeiro js")
+
+//var nome = 'Lucas Müller';
+//alert("Bem-vindo " + nome +"!");
+
+/*
+var idade = 29;
+var idade2 = 10;
+
+alert(idade + idade2);
+console.log(nome);
+console.log(idade2 + idade);
+*/
+
+//var frase = "Japão é um bom país";
+//console.log(frase);
+//console.log(frase.replace("Japão", "Brasil"));
+//console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());
+
+/*
+var lista = ["Maçã","Pêra","Laranja"];
+console.log(lista);
+lista.push("Uva");
+//lista.pop();
+console.log(lista);
+console.log("Comprimento da Lista: " + lista.length + " itens.");
+console.log(lista.reverse());
+console.log(typeof lista);
+var conv = lista.toString();
+console.log("Os elementos " + lista.toString() + " agora são do tipo " + typeof conv + "!");
+var join = lista.join(" - ") ;
+console.log(join + " são " + typeof join + "s");
+*/
+
+/*
+var frutas = [{nome:"Maçã", cor:"Vermelha"},{nome:"Uva", cor:"Roxa"} ];
+console.log(frutas.nome);
+alert(frutas[0].cor);
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+//var idade = prompt("Qual a sua idade?");
+//console.log(idade);
+
+/*
+var idade2 = prompt("Qual a sua idade?")
+if (idade2 >= 18) {
+    alert("Maior de Idade");
+} else {
+    alert("Menor de idade");
+};
+*/
+
+/*
+var count = 0;
+while (count <= 5) { 
+    console.log(count);
+    count++; 
+};
+*/
+
+/*
+var count = 0;
+for (count; count <=5; count++){
+    console.log(count);
+};
+*/
+
+//var date = new Date();
+//alert(date);
