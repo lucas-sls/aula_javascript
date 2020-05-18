422bc0d6128b19cb3887daecf9395d00e7547975
Lucas Fonseca Muller <lucasfm2010@hotmail.com>
2020-05-17 22:08:55 -0300

    Add ´descrição para os codigos do main.js e criando funções de soma simples, replace e boolean no main_2.js

--- js/main.js
+++ js/main.js
@@ -1,72 +1,82 @@
-//alert("Meu primeiro js")
+//COMANDOS INICIAIS
 
-//var nome = 'Lucas Müller';
-//alert("Bem-vindo " + nome +"!");
+//alert("Meu primeiro js") 
+
+//var nome = 'Lucas Müller'; // CRIA VARIÁVEL
+//alert("Bem-vindo " + nome +"!"); // EXIBE MENSAGEM DE ALERTA NO NAVEGADOR
 
 /*
 var idade = 29;
 var idade2 = 10;
 
 alert(idade + idade2);
-console.log(nome);
+console.log(nome); // EXIBE RESULTADO NO CONSOLE DO NAVEGADOR
 console.log(idade2 + idade);
 */
 
 //var frase = "Japão é um bom país";
 //console.log(frase);
-//console.log(frase.replace("Japão", "Brasil"));
-//console.log(frase.toUpperCase());
-//console.log(frase.toLowerCase());
+//console.log(frase.replace("Japão", "Brasil")); // SUBSTITUI O PRIMEIRO PARÂMETRO PELO SEGUNDO!
+//console.log(frase.toUpperCase()); // COLOCA EM LETRAS MAIÚSCULAS
+//console.log(frase.toLowerCase()); // COLCOA EM LETRAS MINÚSCULAS
+
+
+
+// ARRAY E DICIONÁRIO
 
 /*
-var lista = ["Maçã","Pêra","Laranja"];
+var lista = ["Maçã","Pêra","Laranja"]; // COMO SE DECLARA UMA LISTA
 console.log(lista);
-lista.push("Uva");
-//lista.pop();
+lista.push("Uva"); // ADIICONANDO ITEM NA LISTA
+//lista.pop(); // REMOVENDO ULTIMO ITEM DA LISTA
 console.log(lista);
-console.log("Comprimento da Lista: " + lista.length + " itens.");
-console.log(lista.reverse());
-console.log(typeof lista);
-var conv = lista.toString();
+console.log("Comprimento da Lista: " + lista.length + " itens."); // CONCATENAÇÃO E COMPRIMENTO DA LISTA
+console.log(lista.reverse()); // INVERTE A ORDEM DA LISTA
+console.log(typeof lista); // TIPO DE VARIÁVEL
+var conv = lista.toString(); // CONVERTE PARA STRING
 console.log("Os elementos " + lista.toString() + " agora são do tipo " + typeof conv + "!");
-var join = lista.join(" - ") ;
+var join = lista.join(" - ") ; // CONVERTE PARA STRING E JUNTA ITENS COM DELIMITADOR ESPECIFICADO
 console.log(join + " são " + typeof join + "s");
 */
 
 /*
-var frutas = [{nome:"Maçã", cor:"Vermelha"},{nome:"Uva", cor:"Roxa"} ];
+var frutas = [{nome:"Maçã", cor:"Vermelha"},{nome:"Uva", cor:"Roxa"} ]; // CONSTRUÇÃO DE DICIONARIO
 console.log(frutas.nome);
-alert(frutas[0].cor);
+alert(frutas[0].cor); // ACESSANDO COR DO PRIMEIRO ITEM
 console.log(frutas);
-alert(frutas[1].nome);
+alert(frutas[1].nome); // NOME DO SEGUNDO ITEM
 */
 
-//var idade = prompt("Qual a sua idade?");
+
+
+// CONDICIONAIS, LAÇOS DE REPETIÇÃO E DATE
+
+//var idade = prompt("Qual a sua idade?"); // EXIGE INPUT DO USUÁRIO
 //console.log(idade);
 
 /*
 var idade2 = prompt("Qual a sua idade?")
-if (idade2 >= 18) {
+if (idade2 >= 18) {   // ESTRUTURA IF-ELSE
     alert("Maior de Idade");
 } else {
     alert("Menor de idade");
 };
 */
 
-/*
+/* // ESTRUTURA WHILE
 var count = 0;
-while (count <= 5) { 
+while (count <= 5) {
     console.log(count);
-    count++; 
+    count++;
 };
 */
 
-/*
+/* // ESTRUTURA FOR
 var count = 0;
 for (count; count <=5; count++){
     console.log(count);
 };
 */
 
-//var date = new Date();
+//var date = new Date(); // FUNÇÃO DATE
 //alert(date);
---
+++ js/main_2.js
@@ -0,0 +1,35 @@
+//FUNÇÃO DE SOMA SIMPLES
+
+/*
+function soma (n1,n2){
+    return n1 + n2
+}
+alert(soma(10,5))
+*/
+
+
+// FUNÇÃO QUE APLICA REPLACE EM FRASES
+
+/*
+function SetReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+alert(SetReplace("Vai Japão!","Japão","Brasil"));
+*/
+
+//  VALIDAÇÃO DA MAIORIDADE POR BOOLEANO
+
+/*
+var validar = 0;
+function ValidaIdade(idade){
+    if (idade >=18){
+        validar=true
+    }else{
+        validar=false
+    }
+    return validar;
+}
+var idade = prompt("Qual a sua idade? ");
+ValidaIdade(idade);
+console.log(validar)
+*/
