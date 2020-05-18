a6311f6c69eb167bd1f06871e6e125404aa84113
Lucas Fonseca Muller <lucasfm2010@hotmail.com>
2020-05-17 22:19:49 -0300

    Colocando o cod como comentario

--- js/main.js
+++ js/main.js
@@ -5,14 +5,12 @@
 //var nome = 'Lucas Müller'; // CRIA VARIÁVEL
 //alert("Bem-vindo " + nome +"!"); // EXIBE MENSAGEM DE ALERTA NO NAVEGADOR
 
-/*
-var idade = 29;
-var idade2 = 10;
+//var idade = 29;
+//var idade2 = 10;
 
-alert(idade + idade2);
-console.log(nome); // EXIBE RESULTADO NO CONSOLE DO NAVEGADOR
-console.log(idade2 + idade);
-*/
+//alert(idade + idade2);
+//console.log(nome); // EXIBE RESULTADO NO CONSOLE DO NAVEGADOR
+//console.log(idade2 + idade);
 
 //var frase = "Japão é um bom país";
 //console.log(frase);
@@ -24,20 +22,19 @@ console.log(idade2 + idade);
 
 // ARRAY E DICIONÁRIO
 
-/*
-var lista = ["Maçã","Pêra","Laranja"]; // COMO SE DECLARA UMA LISTA
-console.log(lista);
-lista.push("Uva"); // ADIICONANDO ITEM NA LISTA
+
+//var lista = ["Maçã","Pêra","Laranja"]; // COMO SE DECLARA UMA LISTA
+//console.log(lista);
+//lista.push("Uva"); // ADIICONANDO ITEM NA LISTA
 //lista.pop(); // REMOVENDO ULTIMO ITEM DA LISTA
-console.log(lista);
-console.log("Comprimento da Lista: " + lista.length + " itens."); // CONCATENAÇÃO E COMPRIMENTO DA LISTA
-console.log(lista.reverse()); // INVERTE A ORDEM DA LISTA
-console.log(typeof lista); // TIPO DE VARIÁVEL
-var conv = lista.toString(); // CONVERTE PARA STRING
-console.log("Os elementos " + lista.toString() + " agora são do tipo " + typeof conv + "!");
-var join = lista.join(" - ") ; // CONVERTE PARA STRING E JUNTA ITENS COM DELIMITADOR ESPECIFICADO
-console.log(join + " são " + typeof join + "s");
-*/
+//console.log(lista);
+//console.log("Comprimento da Lista: " + lista.length + " itens."); // CONCATENAÇÃO E COMPRIMENTO DA LISTA
+//console.log(lista.reverse()); // INVERTE A ORDEM DA LISTA
+//console.log(typeof lista); // TIPO DE VARIÁVEL
+//var conv = lista.toString(); // CONVERTE PARA STRING
+//console.log("Os elementos " + lista.toString() + " agora são do tipo " + typeof conv + "!");
+//var join = lista.join(" - ") ; // CONVERTE PARA STRING E JUNTA ITENS COM DELIMITADOR ESPECIFICADO
+//console.log(join + " são " + typeof join + "s");
 
 /*
 var frutas = [{nome:"Maçã", cor:"Vermelha"},{nome:"Uva", cor:"Roxa"} ]; // CONSTRUÇÃO DE DICIONARIO
